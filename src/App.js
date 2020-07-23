f88418412a49a8337bf0c53e6266352c12b164ed
prasanth <prashanthmsktm@gmail.com>
2020-07-23 21:08:12 +0530

    test

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Header name='Anu'></Header>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
